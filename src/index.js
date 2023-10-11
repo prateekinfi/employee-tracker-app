efe3d0140fb14503b270f865d5f23b71551f96fa
prateekgarg10 <prateekinfi@gmail.com>
2023-10-11 18:11:48 +0530

    test commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import reducer from './store/reducer';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import reducer from "./store/reducer";
 
 const store = createStore(reducer);
 
@@ -13,10 +13,21 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const channel = new BroadcastChannel("CTIChannel");
+
+channel.onmessage = (e) => {
+  console.log("on message", e);
+};
+
+setInterval(() => {
+  console.log(" setInterval running", e);
+  channel.postMessage("custom message");
+}, 5000);
